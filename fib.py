323c5edbda1674f7ca515dc3f7d08db9058b625a
Omkar Parab <omkar211196@gmail.com>
2019-12-12 19:32:06 +0530

    Add files via upload

--- fib.py
+++ fib.py
@@ -1,8 +1,5 @@
-n=int(input("enter the range: "))
-n1=1
-n2=1
-for i in range(1,n+1):
-    print(n1, end=' ')
-    temp=n1
-    n1=n2
-    n2=temp+n2
+a, b = 0, 1
+for i in range(0, 10):
+    print(a)
+    a, b = b, a+b
+    
